13b4d8ffe469db21bbaabf85de1969d3cebeee41
Matheus Elias <matheus__e@hotmail.com>
2022-09-14 13:03:21 -0300

    Add logic to fetch data

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('should render App', () => {
+    render(<App />);
+    expect(screen.getByText(/Type to search a pokemon/i)).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import AutoComplete from 'components/AutoComplete';
+import useAsync from 'hooks/use-async';
 import './App.css';
 
+const MIN_SEARCH_LENGTH = 2;
+
 function App() {
+  const [showSuggestions, setShowSuggestions] = React.useState(false);
+  const [query, setQuery] = React.useState('');
+  const getSuggestions = () => {
+    return fetch(`${process.env.REACT_APP_URL}/pokemons?name=${query}`).then(
+      (response) => response.json(),
+    );
+  };
+  const {
+    execute,
+    status,
+    value: suggestions,
+    error,
+  } = useAsync<{ name: string; id: number }[] | []>(getSuggestions);
+
+  const onChange = (value: string) => {
+    setQuery(value);
+  };
+
+  React.useEffect(() => {
+    if (!query || query.length < MIN_SEARCH_LENGTH) return;
+
+    const handler = setTimeout(() => {
+      execute();
+    }, 250);
+
+    return () => {
+      clearTimeout(handler);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <AutoComplete
+        onChange={onChange}
+        suggestions={suggestions}
+        loading={status === 'pending'}
+        error={error}
+      />
     </div>
   );
 }
